4c848bc58544eee96acdfedb2a756a215b748e29
Dakota Prince <dakota.prince6@gmail.com>
2026-02-04 14:43:51 -0500

    Added car going around a track, broke the math so it doesnt work properly. speed never changes

--- CarHandler.py
+++ CarHandler.py
@@ -1,7 +1,7 @@
 import yaml
 from dataclasses import dataclass
 from pathlib import Path
-
+import TrackHandler
 
 @dataclass
 class CarSpec:
@@ -14,8 +14,29 @@ class CarState:
     carSpec: CarSpec
     velocity_mps: float = 0.0
     track_position: float = 0.0
+    acceleration_mps: float = 19.6
+    laps: int = 0
 
 def init_car(car_id: int, spec_path: str) -> CarState:
     with open(spec_path,"r") as f:
         raw_spec = yaml.safe_load(f)
     return CarState(car_id = car_id, carSpec = raw_spec)
+
+def run_sim(car: CarState, dt: float, track: TrackHandler.Track, sim_t: float) -> dict | None:
+    
+    if car.velocity_mps < 75.0:
+        car.velocity_mps = car.acceleration_mps * dt
+
+
+
+    car.track_position += car.velocity_mps * dt
+    
+    if car.track_position >= track.lap_length_meter:
+        car.laps += 1
+        car.track_position -= track.lap_length_meter
+
+
+    print(f"Speed: {car.velocity_mps}, Track_position: {car.track_position}")
+    return{"t": sim_t, "car_id":car.car_id, "v_mps":car.velocity_mps, "x_m":car.track_position,
+            "laps":car.laps,}
+
--- sim.py
+++ sim.py
@@ -5,12 +5,20 @@ import math
 import TrackHandler as td
 import CarHandler as ch
 
-DT = 0.05 #roughly 5Hz
-
+Delta_Time = 0.05 #roughly 5Hz
 
 def main():
     track = td.load_track("tracks/cota.yaml", "weather/weather.yaml")
     car = ch.init_car("81", "specs/spec.yaml")
     print(f"{track.name}\n{car.car_id}")
-    
+
+    max_laps = 5
+    sim_t = 0.0
+
+    while True:
+        lap_event = ch.run_sim(car, Delta_Time, track, sim_t)
+
+        time.sleep(Delta_Time)
+
+
 main()
