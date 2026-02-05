0b2fc25d568fbd6e6befd3ea1fb7719814ef2d78
Dakota Prince <dakota.prince6@gmail.com>
2026-02-04 23:05:48 -0500

    fixed the braking, was an issue with wrapping around to the next lap. if the current_pos didnt have another corner it would run past. fighting the latency now.

--- CarHandler.py
+++ CarHandler.py
@@ -41,24 +41,40 @@ def run_sim(car: CarState, dt: float, track: TrackHandler.Track, sim_t: float) -
     #Sections will be commented out for easier manipulation of data and code
 
     #----------Determine future events now!-------
-    if car.track_position > car.braking_zone:
+    if car.track_position >= car.braking_zone:
+        found = False
         for seg in track.segments:
             if seg.type == "corner" and seg.s_meter > car.track_position:
                 car.braking_zone = seg.s_meter
                 car.velo_target = math.sqrt(mu * G * seg.radius_meter)
                 print(car.braking_zone)
+                found = True
                 break
-    car.braking_meter = (car.velocity_mps - car.velo_target) / 2 * a_brake
+        if not found:
+            for seg in track.segments:
+                if seg.type == "corner":
+                    car.braking_zone = seg.s_meter
+                    car.velo_target = math.sqrt(mu * G * seg.radius_meter)
+                    break
+    if car.velocity_mps > car.velo_target:
+        car.braking_meter = (car.velocity_mps**2 - car.velo_target**2) / (2 * a_brake)
+    else:
+        car.braking_meter = 0.0
 
     #----------Variables I think I need-----------
 
     old_pos = car.track_position
+    old_speed = car.velocity_mps
 
     #----------Velocity and distance and braking?--------------
-    if car.braking_zone - car.braking_meter < car.track_position:
+    
+    distance_to_zone = car.braking_zone - car.track_position
+    if distance_to_zone < 0:
+        distance_to_zone += track.lap_length_meter
+    if distance_to_zone <= car.braking_meter + buffer_m:
         car.velocity_mps = max(car.velo_target, car.velocity_mps - a_brake * dt)
+        car.track_position += ((car.velocity_mps+old_speed)/2) * dt
     elif car.velocity_mps < 75.0:
-        old_speed = car.velocity_mps
         car.velocity_mps += car.acceleration_mps * dt
         car.track_position += ((car.velocity_mps+old_speed)/2) * dt
     else:
@@ -73,7 +89,7 @@ def run_sim(car: CarState, dt: float, track: TrackHandler.Track, sim_t: float) -
         print(f"Current Time:{minutes:.0f}:{seconds:.2f}")
         
         #------------calculate actual lap time based on distance past the line----------------i
-        distance_from_start = car.track_position - track.lap_length_meter
+        distance_from_start = track.lap_length_meter - old_pos
         distance_traveled = car.track_position - old_pos
         time_frac = distance_from_start / distance_traveled if distance_traveled > 0 else 0.0
         crossing_time = (sim_t - dt) + time_frac * dt
@@ -82,7 +98,7 @@ def run_sim(car: CarState, dt: float, track: TrackHandler.Track, sim_t: float) -
         car.track_position -= track.lap_length_meter
 
 
-    print(f"Speed: {car.velocity_mps}, Track_position: {car.track_position}")
+    print(f"Speed: {car.velocity_mps}, Track_position: {car.track_position}\nBraking meters and zone: {car.braking_meter} {car.braking_zone}\nVelo_target: {car.velo_target}")
     return{"t": sim_t, "car_id":car.car_id, "v_mps":car.velocity_mps, "x_m":car.track_position,
             "laps":car.laps,}
 
